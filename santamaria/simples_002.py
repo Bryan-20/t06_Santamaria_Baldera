0bb7acb796e411c7f80ea7290f2ac6f07e80388e
Bryan Santamaria <santamaria20b@gmail.com>
2019-12-09 00:50:25 -0500

    fixup! Condicional simples 02

--- santamaria/simples_002.py
+++ santamaria/simples_002.py
@@ -1,27 +1,3 @@
-# Programa 01
-# Escriba un programa que pregunte al usuario su edad y muestre si es mayor o menor de edad.
-import os
-
-# declaracion de variables
-usuario,mayor_O_menor,edad="","",0
-
-# INPUT via OS
-usuario=os.sys.argv[1]
-edad=int(os.sys.argv[2])
-
-# PROCESSINGc
-#Si el nro mostrado es > 18
-# mostrar "Eres mayor de edad!"
-if (edad > 18):
-    print (usuario,"Eres mayor de edad!")
-else:
-    print(usuario,"Eres menor de edad!")
-#fin_if
-
-
-
-
-
 # programa  02
 # Escriba un programa donde el peaton vea el semaforo de color verde, pueda cruzar la calle
 # y si esta rojo le permita esperar
