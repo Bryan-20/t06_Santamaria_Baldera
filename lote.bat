4bae049d5063bb8fa10beb2781b8f5142a5f6f56
Bryan-20 <57965519+bryan-20@users.noreply.github.com>
2019-12-08 23:57:01 -0500

    Condicional simples 01

---
+++ lote.bat
@@ -0,0 +1,5 @@
+python santamaria/simples_01.py "Juan" 19
+python santamaria/simples_01.py "Ana" 20
+python santamaria/simples_01.py "Carlos" 17
+
+
---
+++ santamaria/simples_01.py
@@ -0,0 +1,19 @@
+# Programa 01
+# Escriba un programa que pregunte al usuario su edad y muestre si es mayor o menor de edad.
+import os
+
+# declaracion de variables
+usuario,mayor_O_menor,edad="","",0
+
+# INPUT via OS
+usuario=os.sys.argv[1]
+edad=int(os.sys.argv[2])
+
+# PROCESSING
+#Si el nro mostrado es > 18
+# mostrar "Eres mayor de edad!"
+if (edad > 18):
+    print (usuario,"Eres mayor de edad!")
+else:
+    print(usuario,"Eres menor de edad!")
+#fin_if
